98fae81166586f78b213d4784fb51bf105b693c1
M Ledwold <you@example.com>
2019-03-29 15:15:22 +0000

    first task completed

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        StringReplacer replacer = new StringReplacer();
+        System.out.println(replacer.replaceLetterWithPosition("This NETbuilder assessment is way too easy."));
+    }
+}
---
+++ src/main/java/StringReplacer.java
@@ -0,0 +1,26 @@
+public class StringReplacer {
+
+    String Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+
+    public String replaceLetterWithPosition(String input){
+        String upperCase = input.toUpperCase();
+        char [] inputCharacters = upperCase.toCharArray();
+        char [] alphabetCharacters = Alphabet.toCharArray();
+        String result = "";
+
+         for (int i = 0; i<inputCharacters.length; i++){
+             for (int j = 0; j<alphabetCharacters.length; j++){
+                 if(inputCharacters[i]==alphabetCharacters[j]){
+                     result += (j+1)+" ";
+                 }
+             }
+
+         }
+        return result;
+    }
+
+
+
+}
